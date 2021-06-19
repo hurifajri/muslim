06717adb69aca61eb677156a314dd19fc240d120
Muhamad Fajri <hurifajri@gmail.com>
2021-06-19 14:45:03 +0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
 import React, { ReactNode } from 'react';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
 
-const App = function ({ Component, pageProps }: AppProps): ReactNode {
-  return <Component {...pageProps} />;
-};
+const App = ({ Component, pageProps }: AppProps): ReactNode => (
+  <ChakraProvider>
+    <Component {...pageProps} />
+  </ChakraProvider>
+);
+
 export default App;
