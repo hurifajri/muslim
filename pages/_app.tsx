0099973626cbd976835a787c4b2b71aaa0c675d5
Muhamad Fajri <hurifajri@gmail.com>
2021-06-19 08:38:57 +0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import React, { ReactNode } from "react";
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
-export default MyApp
+const App = function ({ Component, pageProps }: AppProps): ReactNode {
+  return <Component {...pageProps} />;
+};
+export default App;
