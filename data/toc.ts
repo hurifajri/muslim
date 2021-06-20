bc752d5ace45165831acd9372fb7b24c90200639
Muhamad Fajri <hurifajri@gmail.com>
2021-06-20 20:58:40 +0700

    Create table of contents data

---
+++ data/toc.ts
@@ -0,0 +1,13 @@
+const toc = {
+  doa: [
+    { id: 1, title: 'Doa Al-Quran' },
+    { id: 2, title: 'Doa Nabi' },
+  ],
+  dzikir: [
+    { id: 1, title: 'Dzikir Pagi' },
+    { id: 2, title: 'Dzikir Petang' },
+    { id: 3, title: 'Dzikir Setelah Solat' },
+  ],
+};
+
+export default toc;
