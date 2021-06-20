7276d5c0f1694e8292504508ce955dcce053c178
Muhamad Fajri <hurifajri@gmail.com>
2021-06-20 22:49:07 +0700

    Update index.tsx and toc.ts pages

--- pages/api/toc.ts
+++ pages/api/toc.ts
@@ -3,8 +3,8 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 
 // Internal
 import { toc } from '@/data';
-import type { ToC } from '@/interfaces';
+import type { Toc } from '@/interfaces';
 
-export default (req: NextApiRequest, res: NextApiResponse<ToC>): void => {
+export default (req: NextApiRequest, res: NextApiResponse<Toc>): void => {
   res.status(200).json(toc);
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,15 +3,26 @@ import Head from 'next/head';
 import { Stack } from '@chakra-ui/react';
 import React, { ReactNode } from 'react';
 
-const Home = (): ReactNode => (
-  <Stack bgColor="purple.50" height="100vh">
-    <Head>
-      <title>Muslim</title>
-      <meta name="description" content="Muslim" />
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-    <div>tes</div>
-  </Stack>
-);
+// Internal
+import { useToc } from '@/hooks';
+
+const Home = (): ReactNode => {
+  const { contents, isLoading, isError } = useToc();
+
+  if (isError) return <div>Failed to load</div>;
+  if (isLoading) return <div>Loading...</div>;
+  console.log(contents);
+
+  return (
+    <Stack bgColor="purple.50" height="100vh">
+      <Head>
+        <title>Muslim</title>
+        <meta name="description" content="Muslim" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>tes</div>
+    </Stack>
+  );
+};
 
 export default Home;
