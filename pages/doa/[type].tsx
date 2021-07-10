65af524814f9aa87dc21ac99c3f3b77159015a6b
Muhamad Fajri <hurifajri@gmail.com>
2021-07-11 01:58:03 +0700

    Update...

--- pages/doa/[type].tsx
+++ pages/doa/[type].tsx
@@ -4,19 +4,18 @@ import { NextPage } from 'next';
 import { useRouter } from 'next/router';
 
 // Internal
-import { dzikir } from '@data';
+import { doas } from '@data';
 
 const Dzikir: NextPage = () => {
   const { query } = useRouter();
 
   return (
     <Box position="relative">
-      {dzikir
-        .filter(item => ['', query.type].includes(item.time))
-        .map(item => {
-          console.log(item);
+      {doas
+        .filter(doa => doa.types.includes(query.type))
+        .map(doa => {
           return (
-            <Box key={item.id} borderBottom="1px solid rgba(0, 0, 0, 0.1)">
+            <Box key={doa.id} borderBottom="1px solid rgba(0, 0, 0, 0.1)">
               <Flex
                 justify="space-between"
                 borderBottom="1px solid rgba(0, 0, 0, 0.1)"
@@ -24,42 +23,32 @@ const Dzikir: NextPage = () => {
                 align="flex-end"
               >
                 <Text fontWeight="600" mr={2} flex={2.5}>
-                  {item.title}
-                </Text>
-                <Text fontSize="sm" textAlign="right" opacity={0.7} flex={1}>
-                  {item.note}
+                  {doa.title}
                 </Text>
               </Flex>
-              <Box p={4}>
-                <Box
-                  textAlign="right"
-                  fontFamily="'Amiri', serif"
-                  fontWeight="700"
-                  fontSize="2xl"
-                  lineHeight="2.4"
-                  letterSpacing={{ base: '-0.5px', lg: 0 }}
-                  mb={4}
-                  dangerouslySetInnerHTML={{ __html: item.arabic }}
-                />
-                <Text fontStyle="italic" mb={4}>
-                  {item.transliteration}
-                </Text>
-                <Text>
-                  {item.translation}
-                  {item.narrator && ` [${item.narrator}]`}
-                </Text>
-                {item.benefit && (
-                  <Box
-                    py={2}
-                    mt={4}
-                    cursor="pointer"
-                    fontSize="sm"
-                    opacity={0.7}
-                  >
-                    Lihat Keutamaan
+              {doa.contents.map(content => {
+                return (
+                  <Box key={content.id} p={4}>
+                    <Box
+                      textAlign="right"
+                      fontFamily="'Amiri', serif"
+                      fontWeight="700"
+                      fontSize="2xl"
+                      lineHeight="2.4"
+                      letterSpacing={{ base: '-0.5px', lg: 0 }}
+                      mb={4}
+                      dangerouslySetInnerHTML={{ __html: content.arabic }}
+                    />
+                    <Text fontStyle="italic" mb={4}>
+                      {content.transliteration}
+                    </Text>
+                    <Text>
+                      {content.translation}
+                      {content.narrator && ` [${content.narrator}]`}
+                    </Text>
                   </Box>
-                )}
-              </Box>
+                );
+              })}
             </Box>
           );
         })}
--- pages/dzikir/[time].tsx
+++ pages/dzikir/[time].tsx
@@ -4,19 +4,18 @@ import { NextPage } from 'next';
 import { useRouter } from 'next/router';
 
 // Internal
-import { dzikir } from '@data';
+import { dzikirs } from '@data';
 
 const Dzikir: NextPage = () => {
   const { query } = useRouter();
 
   return (
     <Box position="relative">
-      {dzikir
-        .filter(item => ['', query.time].includes(item.time))
-        .map(item => {
-          console.log(item);
+      {dzikirs
+        .filter(dzikir => dzikir.times.includes(query.time))
+        .map(dzikir => {
           return (
-            <Box key={item.id} borderBottom="1px solid rgba(0, 0, 0, 0.1)">
+            <Box key={dzikir.id} borderBottom="1px solid rgba(0, 0, 0, 0.1)">
               <Flex
                 justify="space-between"
                 borderBottom="1px solid rgba(0, 0, 0, 0.1)"
@@ -24,42 +23,46 @@ const Dzikir: NextPage = () => {
                 align="flex-end"
               >
                 <Text fontWeight="600" mr={2} flex={2.5}>
-                  {item.title}
+                  {dzikir.title}
                 </Text>
                 <Text fontSize="sm" textAlign="right" opacity={0.7} flex={1}>
-                  {item.note}
+                  {dzikir.note}
                 </Text>
               </Flex>
-              <Box p={4}>
-                <Box
-                  textAlign="right"
-                  fontFamily="'Amiri', serif"
-                  fontWeight="700"
-                  fontSize="2xl"
-                  lineHeight="2.4"
-                  letterSpacing={{ base: '-0.5px', lg: 0 }}
-                  mb={4}
-                  dangerouslySetInnerHTML={{ __html: item.arabic }}
-                />
-                <Text fontStyle="italic" mb={4}>
-                  {item.transliteration}
-                </Text>
-                <Text>
-                  {item.translation}
-                  {item.narrator && ` [${item.narrator}]`}
-                </Text>
-                {item.benefit && (
-                  <Box
-                    py={2}
-                    mt={4}
-                    cursor="pointer"
-                    fontSize="sm"
-                    opacity={0.7}
-                  >
-                    Lihat Keutamaan
+              {dzikir.contents.map(content => {
+                return (
+                  <Box key={content.id} p={4}>
+                    <Box
+                      textAlign="right"
+                      fontFamily="'Amiri', serif"
+                      fontWeight="700"
+                      fontSize="2xl"
+                      lineHeight="2.4"
+                      letterSpacing={{ base: '-0.5px', lg: 0 }}
+                      mb={4}
+                      dangerouslySetInnerHTML={{ __html: content.arabic }}
+                    />
+                    <Text fontStyle="italic" mb={4}>
+                      {content.transliteration}
+                    </Text>
+                    <Text>
+                      {content.translation}
+                      {content.narrator && ` [${content.narrator}]`}
+                    </Text>
+                    {content.benefits && (
+                      <Box
+                        py={2}
+                        mt={4}
+                        cursor="pointer"
+                        fontSize="sm"
+                        opacity={0.7}
+                      >
+                        Lihat Keutamaan
+                      </Box>
+                    )}
                   </Box>
-                )}
-              </Box>
+                );
+              })}
             </Box>
           );
         })}
