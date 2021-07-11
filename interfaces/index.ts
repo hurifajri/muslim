7cf3ee09c59714d56bfba98cfc51358dd98c6dd3
Muhamad Fajri <hurifajri@gmail.com>
2021-07-11 21:34:44 +0700

    Remove unused interface

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,6 +1,5 @@
 export type { default as iGregDate } from '@interfaces/i-greg-date';
 export type { default as iHijriDate } from '@interfaces/i-hijri-date';
-export type { default as iHome } from '@interfaces/i-home';
 export type { default as iIf } from '@interfaces/i-if';
 export type { default as iLayout } from '@interfaces/i-layout';
 export type { default as iQuote } from '@interfaces/i-quote';
